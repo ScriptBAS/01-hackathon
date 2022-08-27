b53ff3fbb614d095cc46dc2e588c023185629a05
ScriptBas <scriptbas@yandex.ru>
2022-08-27 20:53:47 +0500

    Implemented context menu and background module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+import { ContextMenu } from './menu'
+import { BackgroundModule } from './modules/background.module'
 import './styles.css'
-import { getRandomColor, getRandomNumber } from './utils/utils'
+import { easyMessage } from './modules/easyMessage';
 
+const contextMenu = new ContextMenu('#menu')
+
+document.body.addEventListener('contextmenu', (event)=> {
+  event.preventDefault()
+  contextMenu.open(event)
+})
+
+const backgroundModule = new BackgroundModule('BackgroundModule', 'Сменить фон')
+contextMenu.add(backgroundModule)
+
+const messageModule = new easyMessage('easyMessage', 'Случайный текст')
+contextMenu.add(messageModule)
