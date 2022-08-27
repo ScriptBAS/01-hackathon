f103506bc4f8f85512c0951261536acaa768a03c
Alex <handsome.machine2014@gmail.com>
2022-08-27 16:26:45 +0300

    Fix path to assets

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -3,7 +3,8 @@ import { Module } from "../core/module";
 export class SoundModule extends Module {
   trigger() {
     const audio = new Audio();
-    audio.src = './sound/iphone_5s_ding.mp3';
+    audio.src = '../assets/sound/iphone_5s_ding.mp3';
+    audio.preload(auto);
     audio.play();
   }
 }
